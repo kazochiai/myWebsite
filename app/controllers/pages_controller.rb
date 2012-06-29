bbc577909a7661a4d7fd538a5fa281582dc7b52d
Kazuhito Ochiai <kazu_ochiai@me.com>
2012-06-28 17:43:14 -0700

    vertical menu style, contact form added

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,13 @@
+class PagesController < ApplicationController
+  def home
+    @title = "Home" 
+  end
+
+  def contact
+    @title = "fd"
+  end
+  
+   def about
+     @title = "fsdf"
+   end
+end
